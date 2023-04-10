7bab5ce7ce892530081856a35756b14397cd67c5
Felipe Lecot <47688222+FelipeLecot@users.noreply.github.com>
2023-04-09 23:49:13 -0300

    Update videoscript.py

--- videoscript.py
+++ videoscript.py
@@ -2,8 +2,8 @@ from datetime import datetime
 from moviepy.editor import AudioFileClip
 import voiceover
 
-MAX_WORDS_PER_COMMENT = 100
-MIN_COMMENTS_FOR_FINISH = 4
+MAX_WORDS_PER_COMMENT = 150
+MIN_COMMENTS_FOR_FINISH = 6
 MIN_DURATION = 20
 MAX_DURATION = 58
 
